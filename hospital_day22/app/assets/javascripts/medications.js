de3a7a72ced32bc4975417456823b1ca71ef2371
Malachi Gray <graymd.cfi@gmail.com>
2015-02-05 14:52:05 -0500

    pagination for medications works

--- hospital_day22/app/assets/javascripts/medications.js
+++ hospital_day22/app/assets/javascripts/medications.js
@@ -1,20 +1,12 @@
 $(document).ready(function(){
   $('ul.medication_list > li').hide();
   $('li.section_1').show();
-  $('.link_1').click(function(){
-    $('ul.medication_list > li').hide();
-    $('li.section_1').show();
-  })
-  $('.link_2').click(function(){
-    $('ul.medication_list > li').hide();
-    $('li.section_2').show();
-  })
-  $('.link_3').click(function(){
-    $('ul.medication_list > li').hide();
-    $('li.section_3').show();
-  })
-  $('.link_4').click(function(){
-    $('ul.medication_list > li').hide();
-    $('li.section_4').show();
-  })
+
+$("ul.link_list > li > a").click(function() {
+  $("ul.medication_list > li").hide()
+  var medication_list = $(this).attr("med_num")
+  $("li.section_" + medication_list).show()
+})  
+
+
 })
